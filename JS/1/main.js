5e16c0e6d74bc7189625ec1b0daa2c35caef30b6
Artem Sharapov <artemflashsharapov@gmail.com>
2024-01-23 19:27:17 +0200

    js homework 1

---
+++ JS/1/main.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 7;
+const c = 3;
+const d = 9;
+
+const sum = (a + b + c + d) / 4;
+
+console.log(sum);
+
+const part1 = "F34";
+const part2 = " is";
+const part3 = " the best!"
+
+console.log(part1 + part2 + part3);
