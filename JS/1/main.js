66751bce06151edd63a467693a496ee95f0c14d0
Artem Sharapov <artemflashsharapov@gmail.com>
2024-01-30 18:15:47 +0200

    js hw 2

--- JS/1/main.js
+++ JS/1/main.js
@@ -1,14 +1,27 @@
-const a = 10;
-const b = 7;
-const c = 3;
-const d = 9;
+// Дано 
+const str1 = "Hello";
+// Зробіть останню літеру строки str1 великою, а першу - маленькою
 
-const sum = (a + b + c + d) / 4;
+// Дано 
+const str2 = "artem";
+// напишіть ваше їм'я з великої літери (регістр першої літери)
 
-console.log(sum);
+// Дано
+const n = 12.6436;
+// Залишіть 2 символи після коми, додайте символ $ на початок та кінець
 
-const part1 = "F34";
-const part2 = " is";
-const part3 = " the best!"
+const str3 = "H!el*l000o";
+// Порахуйте кількість літер у str3 (саме літер)
 
-console.log(part1 + part2 + part3);
+console.log(str1.slice(0, 1).toLowerCase(),str1.slice(1, 4),str1.slice(4).toUpperCase()); 
+// почему-то с пробелами 
+const h = str1.slice(0, 1).toLowerCase();
+const ell = str1.slice(1,4);
+const o = str1.slice (4).toUpperCase();
+console.log(h + ell + o);
+
+console.log(str2.replace("a", "A"));
+
+console.log("$" + n.toFixed(2).concat("$"));
+
+console.log(str3.replaceAll("!", "").replaceAll("*", "").replaceAll("0", "").length);
