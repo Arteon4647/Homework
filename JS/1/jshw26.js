3d3c5b9efc0973a93d015a11e63a5c5ca8a78c66
Artem Sharapov <artemflashsharapov@gmail.com>
2024-05-19 16:59:06 +0300

    js hw 26

---
+++ JS/1/jshw26.js
@@ -0,0 +1,7 @@
+const type = prompt("Please enter your type tag");
+alert(`Your type is ${type}`);
+
+const content = prompt("Please enter your tag content");
+alert(`Your tag content is ${content}`);
+
+confirm("Let's continue?");
